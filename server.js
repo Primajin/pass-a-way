e596ac23923fec1e67247520d785e9f26954d05b
Jannis Hell <Primajin@users.noreply.github.com>
2022-01-28 15:41:40 +0100

    :test_tube: add tests

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ context.fillStyle = '#3574d4';
 const text = 'Hello, World!';
 
 const textWidth = context.measureText(text).width;
-context.fillRect(600 - textWidth / 2 - 10, 170 - 5, textWidth + 20, 120);
+context.fillRect(600 - (textWidth / 2) - 10, 170 - 5, textWidth + 20, 120);
 context.fillStyle = '#fff';
 context.fillText(text, 600, 170);
 
