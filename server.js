713365923aa4f1d3c9eb5e4aba0b1735efae3487
Jannis Hell <Primajin@users.noreply.github.com>
2022-01-28 16:49:30 +0100

    :8ball: use different random function

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ import canvasPKG from 'canvas';
 
 const {createCanvas} = canvasPKG;
 
-const getRandomInt = max => Math.floor(Math.random() * max);
+/**
+ * Generates random number between min and max (inclusive)
+ * @param {number} [max=255] - Max value
+ * @param {number} [min=0] - Min value
+ * @returns {number}
+ */
+const getRandomIntInclusive = (max = 255, min = 0) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor((Math.random() * (max - min + 1)) + min); // The maximum is inclusive and the minimum is inclusive
+};
 
 const width = 1200;
 const height = 630;
@@ -15,8 +25,8 @@ context.fillStyle = '#000';
 context.fillRect(0, 0, width, height);
 
 for (let i = 500; i--;) {
-	context.fillStyle = 'rgba(' + getRandomInt(256) + ',' + getRandomInt(256) + ',' + getRandomInt(256) + ',' + (getRandomInt(256) / 255) + ')';
-	context.fillRect(getRandomInt(width), getRandomInt(height), 1, 1);
+	context.fillStyle = 'rgba(' + getRandomIntInclusive() + ',' + getRandomIntInclusive() + ',' + getRandomIntInclusive() + ',' + (getRandomIntInclusive(255, 150) / 255) + ')';
+	context.fillRect(getRandomIntInclusive(width), getRandomIntInclusive(height), 1, 1);
 }
 
 context.font = 'bold 70pt Sans';
