8a7daf83983d57b2a85840c9be0410dd9a1864f5
Jannis Hell <Primajin@users.noreply.github.com>
2022-02-20 16:29:58 +0100

    :building_construction: bring esbuild back

---
+++ scripts/build.js
@@ -0,0 +1,48 @@
+import {build} from 'esbuild';
+
+const parameters = process.argv.slice(2); // eslint-disable-line node/prefer-global/process
+
+/**
+ * Iterate through the parameters and set the build parameters
+ * @param {Array} parameters
+ * @returns {Object}
+ */
+const getBuildParameters = parameters => {
+	const buildParameters = {
+		bundle: true,
+		entryPoints: ['src/browser/index.js'],
+		external: [],
+		outfile: 'dist/browser/index.js',
+		platform: 'browser',
+		watch: false,
+	};
+
+	// Iterate through the parameters and overwrite the default options
+	for (const parameter of parameters) {
+		const [key, value] = parameter.split('=');
+		buildParameters[key.replace('--', '')] = value ?? true;
+	}
+
+	if (buildParameters.watch) {
+		buildParameters.watch = {
+			onRebuild(error) {
+				if (error) {
+					console.error('watch build failed:', error);
+				} else {
+					console.log('Build complete', new Date().toLocaleString());
+				}
+			},
+		};
+		console.log('Watching for changes...');
+	}
+
+	return buildParameters;
+};
+
+build({
+	...getBuildParameters(parameters),
+}).then(() => {
+	console.log('Build complete', new Date().toLocaleString());
+}).catch(error => {
+	console.error(error);
+});
